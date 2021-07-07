b5d55aca6c88b09b37f2592db2fad7d31069f5d6
JunkFood <48719632+JunkFood@users.noreply.github.com>
2021-07-07 14:44:19 +0900

    Add files via upload

--- network/cc.sh
+++ network/cc.sh
@@ -11,18 +11,18 @@ version=$2
 set -ev
 
 #chaincode install
-docker exec cli peer chaincode install -n teamate -v $version -p github.com/teamate
+docker exec cli peer chaincode install -n newcloth -v $version -p github.com/newcloth
 #chaincode instatiate
-docker exec cli peer chaincode $instruction -n teamate -v $version -C mychannel -c '{"Args":[]}' -P 'OR ("Org1MSP.member")'
+docker exec cli peer chaincode $instruction -n newcloth -v $version -C mychannel -c '{"Args":[]}' -P 'OR ("Org1MSP.member")'
 sleep 5
 #chaincode invoke user1
-docker exec cli peer chaincode invoke -n teamate -C mychannel -c '{"Args":["addUser","user1", "test"]}'
+docker exec cli peer chaincode invoke -n newcloth -C mychannel -c '{"Args":["addDonate","user1", "test"]}'
 sleep 5
 #chaincode query user1
-docker exec cli peer chaincode query -n teamate -C mychannel -c '{"Args":["readRating","user1"]}'
+docker exec cli peer chaincode query -n newcloth -C mychannel -c '{"Args":["readDonate","user1"]}'
 
 #chaincode invoke add rating
-docker exec cli peer chaincode invoke -n teamate -C mychannel -c '{"Args":["addRating","user1","수거신청"]}'
+docker exec cli peer chaincode invoke -n newcloth -C mychannel -c '{"Args":["changeState","user1","수거신청"]}'
 sleep 5
 
 echo '-------------------------------------END-------------------------------------'
